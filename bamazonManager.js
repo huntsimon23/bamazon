86c1fe10dc48300dd184e6f653c5b98bc7ef07b5
huntsimon23 <hunt.simon23@gmail.com>
2019-05-18 03:55:30 -0400

    bamazonManager app fully functional

--- bamazonManager.js
+++ bamazonManager.js
@@ -27,7 +27,7 @@ function managerStuff() {
         choices: managerList,
         message: "Hello boss!  What would you like to do?"
     }).then(function(answers) {
-        switch (answers){
+        switch (answers.managerList){
             case "View Products for Sale":
             productsForSale();
                 break;
@@ -55,6 +55,7 @@ function productsForSale() {
         if (err) throw err;
         var table = cTable.getTable(res);
         console.log("Here are the current products for sale at Bamazon:")
+        console.log("- - - - - - - - - - - - - - - - - ");
         console.log(table);
         managerStuff();
     });
@@ -65,9 +66,101 @@ function lowInventory() {
         if (err) throw err;
         var table = cTable.getTable(res);
         console.log("There are 5 or less of these items...consider adding to inventory!");
+        console.log("- - - - - - - - - - - - - - - - - ");
         console.log(table);
         managerStuff();
     });
 };
 
-function addToInventory()
+function addToInventory() {
+    connection.query("SELECT * FROM products", function(err, res){
+        if (err) throw err;
+        var table = cTable.getTable(res);
+        var products = [];
+        console.log("Here are the current inventory levels:");
+        console.log("- - - - - - - - - - - - - - - - - ");
+        console.log(table);
+        for (i = 0; i < res.length; i++) {
+            products.push(res[i].product_name);
+        };
+        inquirer.prompt([{
+            type: "list",
+            name: "productList",
+            message: "WHich product would you like to offer more of?",
+            choices: products
+        }, {
+            type: "input",
+            name: "stockQuantity",
+            message: "How many would you like to order for restock?",
+            validate:  function(val){
+                if(isNaN(val)==false){
+                    return true;
+                } else {
+                    return false;
+                };
+            },
+        }]).then(function(ans) {
+            connection.query("UPDATE products SET stock_quantity = stock_quantity + '" + ans.stockQuantity + "'WHERE ?", {product_name: ans.productList}, function(err, res) {
+            if (err) throw err;
+            console.log("- - - - - - - - - - - - - - - - - ");
+            console.log("Stock is being shipped! " + ans.stockQuantity + " " + ans.productList + "'s are on the way.");    
+            managerStuff();
+            });
+        }).catch(function(error) {
+            if (error) throw error;
+        });
+    });
+};
+
+function addNewProduct() {
+    inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "What new product would you like Bamazon to sell?"
+      },
+      {
+        name: "department",
+        type: "input",
+        message: "What department will it be listed under?"
+      },
+      {
+        name: "price",
+        type: "input",
+        message: "How much will it be sold for?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      },
+      {
+        name: "quantity",
+        type: "input",
+        message: "How many will Bamazon stock?",
+        validate: function(value) {
+          if (isNaN(value) === false) {
+            return true;
+          }
+          return false;
+        }
+      }
+    ]).then(function(answer) {
+      connection.query(
+        "INSERT INTO products SET ?",
+        {
+          product_name: answer.name,
+          department_name: answer.department,
+          price: answer.price,
+          stock_quantity: answer.quantity
+        },
+        function(err) {
+          if (err) throw err;
+          console.log("You have successfully added the following product to Bamazon:" + cTable.getTable(answer));
+          managerStuff();
+        }
+      );
+    });
+};
